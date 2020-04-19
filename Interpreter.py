fe9198cfcc85e985a1ca02f586ab5019bff68fd9
Rsambeek <rickvansambeek@gmail.com>
2020-04-19 12:10:35 +0200

    fairly sure base variables are working

--- Interpreter.py
+++ Interpreter.py
@@ -5,7 +5,7 @@ inputCode = open("codeFile.penis", "r")
 
 class Interpreter:
     def __init__(self):
-        self.tokenTypes = {"identifier": [],
+        self.tokenTypes = {"identifier": {},
                            "keyword": {"if": [], "while": [], "return": [], "int": int, "float": float, "char": chr, "String": str},
                            "separator": [],
                            "operator": ["=", "+"],
@@ -17,18 +17,22 @@ class Interpreter:
         self.error = ""
 
     def defineVariable(self, keywordToken, identifierToken) -> None:
-        print(self.tokens)
         if identifierToken[0] in self.tokenTypes["identifier"]:
             self.error = identifierToken[0] + " is already defined"
             return
         else:
-            self.tokenTypes["identifier"][identifierToken[0]] = self.tokenTypes["keyword"][keywordToken[0]]()
+            self.tokenTypes["identifier"][identifierToken[0]] = self.tokenTypes["keyword"][keywordToken[0]]
+            self.variables[identifierToken[0]] = self.tokenTypes["identifier"][identifierToken[0]]()
             return
 
-    def assignVariable(self, identifierToken, literalToken) -> None:
-        if identifierToken[0] in self.tokenTypes["keyword"]:
-            self.tokenTypes["identifier"][identifierToken[0]] = self.tokenTypes["keyword"][identifierToken[0]](literalToken[0])
-            return
+    def assignVariable(self, identifierToken, valueToken) -> None:
+        if identifierToken[0] in self.tokenTypes["identifier"]:
+            if valueToken[1] == "literal":
+                self.variables[identifierToken[0]] = self.tokenTypes["identifier"][identifierToken[0]](valueToken[0])
+                return
+            elif valueToken[1] == "identifier":
+                self.variables[identifierToken[0]] = self.tokenTypes["identifier"][identifierToken[0]](self.variables[valueToken[0]])
+                return
 
         else:
             self.error = "bad assignment"
@@ -92,34 +96,43 @@ class Interpreter:
                         if (len(currentToken[0]) == 0) or currentToken[-1].isalpha():
                             currentToken[0] += line[0]
                         else:
+                            print("its me alpha")
                             return False
 
                     elif line[0].isdigit():
-                        if (len(currentToken[0]) == 0) or currentToken[-1].isdigit():
+                        if (len(currentToken[0]) == 0) or currentToken[0][-1].isdigit():
                             currentToken[0] += line[0]
                             if len(currentToken) == 1:
                                 currentToken.append("literal")
                         else:
+                            print("its me digit")
                             return False
 
                 line = line[1:]
 
-            tokenIndex = 0
-            while tokenIndex < len(self.tokens):
-                if self.tokens[tokenIndex][1] == "keyword":
-                    self.defineVariable(self.tokens[tokenIndex], self.tokens[tokenIndex + 1])
-                    tokenIndex += 1
+        tokenIndex = 0
+        while tokenIndex < len(self.tokens):
+            if self.tokens[tokenIndex][1] == "keyword":
+                self.defineVariable(self.tokens[tokenIndex], self.tokens[tokenIndex + 1])
+                tokenIndex += 1
 
-                if self.tokens[tokenIndex][1] == "operator":
-                    self.assignVariable(self.tokens[tokenIndex - 1], self.tokens[tokenIndex + 1])
-                    tokenIndex += 1
+            elif self.tokens[tokenIndex][1] == "operator":
+                self.assignVariable(self.tokens[tokenIndex - 1], self.tokens[tokenIndex + 1])
+                tokenIndex += 1
 
-                # elif (tokens[index] == "=" and tokens[index-1] != None):
-                else:
-                    print("Failed recognizing token: " + self.tokens[0][0])
-                    return False
+            # elif self.tokens[tokenIndex][1] == "identifier":
+            #     self.
 
+            # elif (tokens[index] == "=" and tokens[index-1] != None):
+            else:
+                print("Failed recognizing token: " + self.tokens[0][0])
+                return False
+            tokenIndex += 1
         print(self.tokenTypes["identifier"])
+        print(self.tokens)
+        print(self.variables)
+        return True
 
 interpreter = Interpreter()
-interpreter.tokenizeCode(inputCode)
+if not interpreter.tokenizeCode(inputCode) :
+    print("Something went wrong")
